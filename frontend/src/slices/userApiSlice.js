65954d85f19790baa882edcbaee1df2eedbe542f
FARSEENA <farseenakazeez187@gmail.com>
2026-06-06 21:21:09 +0530

    userApiSlice error corrected

--- frontend/src/slices/userApiSlice.js
+++ frontend/src/slices/userApiSlice.js
@@ -4,7 +4,7 @@ const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     loginUser: builder.mutation({
       query: (data) => ({
-        url: "/api/user/login",
+        url: "/api/user",
         method: "POST",
         body: data,
       }),
